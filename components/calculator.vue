eaa08144bdf5310f75e091b64a6e4bd39cacd52d
lyshka <lyshka@lyshka.lyshka>
2023-01-31 20:13:38 +0100

    problem mep

--- components/calculator.vue
+++ components/calculator.vue
@@ -10,13 +10,22 @@
       </div>
     </div>
 
-    <div class="mr-3 ml-3">
-      <div class="bg-zinc-800 p-96 mb-3 border-4 border-zinc-500 rounded">
+    <div>
+      <div class="mr-3 ml-3 bg-zinc-800 p-96 mb-3 border-4 border-zinc-500 rounded">
         <div class="text-white font-bold text-center text-5xl relative bottom-80">
           mYminicaLc
         </div>
 
-        <input class="w-80 h-14 border-2 border-zinc-500 rounded relative bottom-80">
+        <div class="mx-auto">
+          <input class="w-80 h-14 border-2 border-zinc-500 rounded relative bottom-80 text-right text-2xl">
+        </div>
+
+        <div class="bg-zinc-700 w-2/5 h-96 mb-3 border-4 border-zinc-400 rounded"></div>
+
+        <div class="text-white text-sm text-center">
+          <div class="font-bold">- Hello, I am a minimalistic calculator -</div>
+          ( A smawl project to learn <a href="https://tailwindcss.com/" class="underline text-sky-600">Tailwind CSS</a>, <a href="https://vuejs.org/" class="underline text-sky-600">Vue.js</a> and <a href="https://nuxt.com/" class="underline text-sky-600">Nuxt.js</a> )
+        </div>
       </div>
     </div>
 
