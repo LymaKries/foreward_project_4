e4d41998f8e093900ca0ec8563f1ccd6bbe7b476
lyshka <lyshka@lyshka.lyshka>
2023-02-01 16:20:03 +0100

    succeed to recreate things like this morning, except the center of input + calculator

---
+++ components/Calculator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full h-full bg-zinc-800 border-4 border-zinc-500 rounded">
+    <div class="h-full flex flex-col justify-evenly content-evenly">
+      <div class="text-white font-bold text-center text-5xl bottom-80">
+        mYminicaLc
+      </div>
+
+      <div class="w-full flex flex-col">
+        <div class="w-80 h-14 bg-white border-2 border-zinc-500 rounded bottom-80"></div>
+        <div class="w-96 h-14 bg-zinc-700 mb-3 border-4 border-zinc-400 rounded"></div>
+      </div>
+
+      <div class="text-white text-sm text-center">
+        <div class="font-bold">
+          - Hello, I am a minimalistic calculator -
+        </div>
+        <div>
+          ( A smawl project to learn <a href="https://tailwindcss.com/" class="underline text-sky-600">Tailwind CSS</a>, <a href="https://vuejs.org/" class="underline text-sky-600">Vue.js</a> and <a href="https://nuxt.com/" class="underline text-sky-600">Nuxt.js</a> )
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <calculator />
+  <div class="w-screen h-screen p-3">
+    <div class="h-full flex flex-col">
+      <Title />
+      <Calculator />
+    </div>
+  </div>
 </template>
 
 <script>
-import Calculator from "../components/calculator.vue";
+import Calculator from "../components/Calculator.vue";
+import Title from "../components/Title.vue";
 
 export default {
   name: 'IndexPage',
-  components: {Calculator}
+  components: {Title, Calculator}
 }
 </script>
