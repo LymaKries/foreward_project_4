3bf135a04b4b75bee918ede69cb498da1d8b6c58
lyshka <lyshka@lyshka.lyshka>
2023-02-05 20:00:21 +0100

    responsive in progress + try to correct host gitpage

--- components/BottomText.vue
+++ components/BottomText.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="text-white text-sm text-center">
+  <div class="text-white text-xs sm:text-sm text-center">
     <div class="font-bold">
       - Hello, I am a minimalistic calculator -
     </div>
-    <div>
+    <div class="text-xs sm:text-sm">
       ( A smawl project to learn <a href="https://tailwindcss.com/" class="underline text-sky-600">Tailwind CSS</a>, <a href="https://vuejs.org/" class="underline text-sky-600">Vue.js</a> and <a href="https://nuxt.com/" class="underline text-sky-600">Nuxt.js</a> )
     </div>
   </div>
--- components/Calculator.vue
+++ components/Calculator.vue
@@ -3,10 +3,10 @@
     <div class="h-full flex flex-col justify-evenly content-evenly">
       <Title />
 
-      <div class="w-full flex flex-col items-center">
-        <div class="pr-1 mb-3 bg-gray-200 border-2 border-zinc-500 rounded bottom-80 text-3xl flex items-center justify-end shrink w-80 h-14">{{input}}</div>
+      <div class="w-full flex flex-col items-center ">
+        <div class="w-56 h-10 pr-1 mb-3 bg-gray-200 border-2 border-zinc-500 rounded bottom-80 text-3xl flex items-center justify-end sm:w-80 sm:h-14 sm:te">{{input}}</div>
 
-        <div class="flex w-62 h-96 bg-neutral-700 mb-3 border-4 border-zinc-400 rounded-lg sm:w-80 h-96">
+        <div class="w-56 h-80 bg-neutral-700 mb-3 border-4 border-zinc-400 rounded-lg sm:w-80 sm:h-96">
           <div class="grid grid-cols-4 grid-rows-5 gap-2 p-3">
             <BigButton class="bg-neutral-300 border-amber-500 text-neutral-400 pl-3"
                        big_button="AC"
@@ -174,7 +174,7 @@ export default {
         }
 
         if (this.operation[i] === ".") {
-          this.input += "w0";
+          this.input += "0";
           this.operation += "0";
           this.result += 0.0;
           return;
